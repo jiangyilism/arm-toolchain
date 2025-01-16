4dab55a831d3d0e9942812c5be8cfcab3be041c9
Hugo da Silva <hugo.silva@arm.com>
2025-01-16 13:15:14 +0000

    Revert "Add scripts to run builds and tests for ATfE (#29)"
    This reverts commit 61b435307f886128ca471bb382e45882a604011f.

--- arm-software/embedded/scripts/test.bat
+++
@@ -1,7 +0,0 @@
-
-REM A bat script to run the tests from the Arm Toolchain for Embedded
-
-vcvars64.bat
-
-cd arm-toolchain\build
-ninja check-llvm-toolchain
--- arm-software/embedded/scripts/test.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# A bash script to run the tests from the Arm Toolchain for Embedded
-
-cd arm-toolchain/build
-ninja check-llvm-toolchain
