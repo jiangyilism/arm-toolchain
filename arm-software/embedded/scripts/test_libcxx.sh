a30eeb93ee2ad1d34b39f71b6a316ab58a816463
Lucas Duarte Prates <lucas.prates@arm.com>
2025-02-17 11:56:10 +0000

    [ATfE] Add copyright and license banner to build and test scripts (#95)

--- arm-software/embedded/scripts/test_libcxx.sh
+++ arm-software/embedded/scripts/test_libcxx.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# Copyright (c) 2025, Arm Limited and affiliates.
+# Part of the Arm Toolchain project, under the Apache License v2.0 with LLVM Exceptions.
+# See https://llvm.org/LICENSE.txt for license information.
+# SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+
 # A bash script to run the libcxx tests from the Arm Toolchain for Embedded.
 
 # These tests are not included in the generic test.sh script due to the very
