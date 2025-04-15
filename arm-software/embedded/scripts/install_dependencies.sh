e56c6b65c07b899d9d3e888c89f91a4e7323f8a1
Hugo da Silva <hugo.silva@arm.com>
2025-04-15 16:05:35 +0100

    Add nightly build and test workflow for ATfE (#276)
    Add nightly build and test workflow for ATfE
    
    -  Initial support for Linux Ubuntu 22.04
    -  Run all build scripts in parallel
    -  Add install dependencies script
    -  Update source for Newlib. Use GitHub mirror

---
+++ arm-software/embedded/scripts/install_dependencies.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Copyright (c) 2025, Arm Limited and affiliates.
+# Part of the Arm Toolchain project, under the Apache License v2.0 with LLVM Exceptions.
+# See https://llvm.org/LICENSE.txt for license information.
+# SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+#
+# This script installs the essential build dependencies for ATfE.
+
+set -e
+
+sudo apt-get update && sudo apt-get install -y --no-install-recommends \
+    cmake=3.22.1-1ubuntu1.22.04.2 \
+    ninja-build=1.10.1-1 \
+    clang=1:14.0-55~exp2 \
+    python3-pip \
+    python3-setuptools
+
+# Upgrade pip and install meson with a pinned version
+python3 -m pip install --upgrade pip
+python3 -m pip install meson==1.2.3
