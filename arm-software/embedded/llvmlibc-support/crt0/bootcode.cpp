jiangyilism/arm-toolchain

commit ae2115da97fd58dddcc75306afea97286ff29fa8

[ATfE] Use new bootcode in llvmlibc-support (#394)
This eliminates the need for vector.c, as this is linked in the
bootcode. This still needs a bit of work to be bulletproof, but for the
samples, it works fine.

arm-software/embedded/llvmlibc-support/crt0/bootcode.cpp
@@ -1,0 +1,90 @@
+//
+// Copyright (c) 2025, Arm Limited and affiliates.
+//
+// Part of the Arm Toolchain project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+
+#include <stdint.h>
+
+#if __ARM_ARCH_PROFILE == 'A' || __ARM_ARCH_PROFILE == 'R'
+#include "exceptions_a.h"
+#include "memory_a.h"
+#include "misc_a.h"
+#include "system_registers_a.h"
+#elif __ARM_ARCH_PROFILE == 'M'
+#include "exceptions_m.h"
+#include "memory_m.h"
+#include "misc_m.h"
+#include "system_registers_m.h"
+#endif
+
+using namespace bootcode;
+
+int main(int argc, const char **argv);
+extern "C" void __libc_init_array();
+extern "C" void _platform_init();
+
+// Required for exit() on baremetal
+// TODO: This is temporary, remove once implemented upstream
+extern "C" [[gnu::weak]] void __cxa_finalize(void *) {}
+
+extern char __data_source[];
+extern char __data_start[];
+extern char __data_size[];
+extern char __bss_start[];
+extern char __bss_size[];
+
+#ifdef __ARM_FEATURE_PAUTH
+// Disable pointer authentication, as it isn't enabled until misc::setup meaning
+// the PAC at the beginning would do nothing so the AUT at the end would fail.
+__attribute__((target("branch-protection=none")))
+#endif
+extern "C" void __startup() {
+  exceptions::setup();
+  memory::setup();
+  misc::setup();
+
+  // Perform the equivalent of scatterloading
+  memcpy(__data_start, __data_source, (uintptr_t)__data_size);
+  memset(__bss_start, '\0', (uintptr_t)__bss_size);
+
+  memory::enable_cache();
+  __libc_init_array();
+  _platform_init();
+  exit(main(0, 0));
+}
+
+// The entry point sets sp and branches to the main startup function.
+#ifdef __ARM_ARCH_ISA_ARM
+// If the target supports the A32 instruction set then the entry point has to
+// use it.
+__attribute__((target("arm")))
+#endif
+__attribute__((naked)) extern "C" void _start() {
+#if __ARM_ARCH_PROFILE != 'M' && __ARM_ARCH >= 8 && !defined(__ARM_ARCH_ISA_A64)
+  // Check if we're in hypervisor mode
+  asm("mrs r0, CPSR\n\t"
+      "and r0, r0, #0x1f\n\t"
+      "cmp r0, #0x1a\n\t"
+      "bne after_mode_switch"
+      :
+      :
+      : "r0");
+  // If so switch to svc mode, which is what we would have started execution in
+  // if we didn't have hypervisor mode.
+  asm("adr r0, after_mode_switch\n\t"
+      "msr ELR_hyp, r0"
+      :
+      :
+      : "r0");
+  asm("msr SPSR_hyp, %0" : : "r"(0x13));
+  asm("eret");
+  asm("after_mode_switch:");
+#endif
+
+  // Configured through linker script defined symbols
+  asm("mov sp, %0" : : "r"(&__stack));
+  asm("bl %0" : : "X"(__startup));
+}

arm-software/embedded/llvmlibc-samples/src/baremetal-semihosting-llvmlibc/make.bat
@@ -40,6 +40,6 @@ if exist hello.hex del /q hello.hex
 @exit /B 1
 
 :build_fn
-%BIN_PATH%\clang.exe --config=llvmlibc.cfg --target=armv6m-none-eabi -mfloat-abi=soft -march=armv6m -mfpu=none -nostartfiles -lcrt0-semihost -lsemihost -fno-exceptions -fno-rtti -g -T microbit-llvmlibc.ld -lm -o hello.elf hello.c vector.c
+%BIN_PATH%\clang.exe --config=llvmlibc.cfg --target=armv6m-none-eabi -mfloat-abi=soft -march=armv6m -mfpu=none -nostartfiles -lcrt0-semihost -lsemihost -fno-exceptions -fno-rtti -g -T microbit-llvmlibc.ld -lm -o hello.elf hello.c
 %BIN_PATH%\llvm-objcopy.exe -O ihex hello.elf hello.hex
 @exit /B

arm-software/embedded/llvmlibc-samples/src/baremetal-semihosting-llvmlibc/vector.c
@@ -1,47 +1,0 @@
-// Copyright (c) 2024-2025, Arm Limited and affiliates.
-//
-// Part of the Arm Toolchain project, under the Apache License v2.0 with LLVM Exceptions.
-// See https://llvm.org/LICENSE.txt for license information.
-// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
-
-#include <stdlib.h>
-#include <stdint.h>
-
-/* Adapted from https://developer.arm.com/documentation/107565/0101/Use-case-examples/Generic-Information/What-is-inside-a-program-image-/Vector-table */
-
-extern void __llvm_libc_exit();
-
-extern uint8_t __stack[];
-
-extern void _start(void);
-
-
-void NMI_Handler() {}
-void HardFault_Handler() { __llvm_libc_exit(); }
-void MemManage_Handler() { __llvm_libc_exit(); }
-void BusFault_Handler() { __llvm_libc_exit(); }
-void UsageFault_Handler() { __llvm_libc_exit(); }
-void SVC_Handler() {}
-void DebugMon_Handler() {}
-void PendSV_Handler() {}
-void SysTick_Handler() {}
-typedef void(*VECTOR_TABLE_Type)(void);
-const VECTOR_TABLE_Type __VECTOR_TABLE[496] __attribute__((section(".vectors"))) __attribute__((aligned(128))) = {
-  (VECTOR_TABLE_Type)__stack,               /*     Initial Stack Pointer */
-  _start,                                   /*     Reset Handler */
-  NMI_Handler,                              /*     NMI Handler */
-  HardFault_Handler,                        /*     Hard Fault Handler */
-  MemManage_Handler,                        /*     MPU Fault Handler */
-  BusFault_Handler,                         /*     Bus Fault Handler */
-  UsageFault_Handler,                       /*     Usage Fault Handler */
-  0,                                        /*     Reserved */
-  0,                                        /*     Reserved */
-  0,                                        /*     Reserved */
-  0,                                        /*     Reserved */
-  SVC_Handler,                              /*     SVC Handler */
-  DebugMon_Handler,                         /*     Debug Monitor Handler */
-  0,                                        /*     Reserved */
-  PendSV_Handler,                           /*     PendSV Handler */
-  SysTick_Handler,                          /*     SysTick Handler */
-  /* Unused */
-};

arm-software/embedded/llvmlibc-samples/src/cpp-baremetal-semihosting-llvmlibc/Makefile
@@ -10,8 +10,7 @@ include ../../Makefile.conf
 build: hello.elf
 
 hello.elf: *.cpp
-	$(BIN_PATH)/clang --config=llvmlibc.cfg $(MICROBIT_TARGET) -c *.c
-	$(BIN_PATH)/clang++ --config=llvmlibc.cfg $(MICROBIT_TARGET) $(CRT_SEMIHOST) -lm -g -T microbit-llvmlibc.ld *.o -o hello.elf $^
+	$(BIN_PATH)/clang++ --config=llvmlibc.cfg $(MICROBIT_TARGET) $(CRT_SEMIHOST) -lm -g -T microbit-llvmlibc.ld -o hello.elf $^
 
 %.hex: %.elf
 	$(BIN_PATH)/llvm-objcopy -O ihex $< $@

arm-software/embedded/llvmlibc-samples/src/cpp-baremetal-semihosting-llvmlibc/make.bat
@@ -40,7 +40,6 @@ if exist hello.hex del /q hello.hex
 @exit /B 1
 
 :build_fn
-%BIN_PATH%\clang.exe --config=llvmlibc.cfg --target=armv6m-none-eabi -mfloat-abi=soft -march=armv6m -mfpu=none -g -c vector.c
-%BIN_PATH%\clang++.exe --config=llvmlibc.cfg --target=armv6m-none-eabi -mfloat-abi=soft -march=armv6m -mfpu=none -nostartfiles -lcrt0-semihost -lsemihost -fno-exceptions -fno-rtti -g -T microbit-llvmlibc.ld -lm -o hello.elf hello.cpp vector.o
+%BIN_PATH%\clang++.exe --config=llvmlibc.cfg --target=armv6m-none-eabi -mfloat-abi=soft -march=armv6m -mfpu=none -nostartfiles -lcrt0-semihost -lsemihost -fno-exceptions -fno-rtti -g -T microbit-llvmlibc.ld -lm -o hello.elf hello.cpp
 %BIN_PATH%\llvm-objcopy.exe -O ihex hello.elf hello.hex
 @exit /B

arm-software/embedded/llvmlibc-samples/src/cpp-baremetal-semihosting-llvmlibc/vector.c
@@ -1,47 +1,0 @@
-// Copyright (c) 2024-2025, Arm Limited and affiliates.
-//
-// Part of the Arm Toolchain project, under the Apache License v2.0 with LLVM Exceptions.
-// See https://llvm.org/LICENSE.txt for license information.
-// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
-
-#include <stdlib.h>
-#include <stdint.h>
-
-/* Adapted from https://developer.arm.com/documentation/107565/0101/Use-case-examples/Generic-Information/What-is-inside-a-program-image-/Vector-table */
-
-extern void __llvm_libc_exit();
-
-extern uint8_t __stack[];
-
-extern void _start(void);
-
-
-void NMI_Handler() {}
-void HardFault_Handler() { __llvm_libc_exit(); }
-void MemManage_Handler() { __llvm_libc_exit(); }
-void BusFault_Handler() { __llvm_libc_exit(); }
-void UsageFault_Handler() { __llvm_libc_exit(); }
-void SVC_Handler() {}
-void DebugMon_Handler() {}
-void PendSV_Handler() {}
-void SysTick_Handler() {}
-typedef void(*VECTOR_TABLE_Type)(void);
-const VECTOR_TABLE_Type __VECTOR_TABLE[496] __attribute__((section(".vectors"))) __attribute__((aligned(128))) = {
-  (VECTOR_TABLE_Type)__stack,               /*     Initial Stack Pointer */
-  _start,                                   /*     Reset Handler */
-  NMI_Handler,                              /*     NMI Handler */
-  HardFault_Handler,                        /*     Hard Fault Handler */
-  MemManage_Handler,                        /*     MPU Fault Handler */
-  BusFault_Handler,                         /*     Bus Fault Handler */
-  UsageFault_Handler,                       /*     Usage Fault Handler */
-  0,                                        /*     Reserved */
-  0,                                        /*     Reserved */
-  0,                                        /*     Reserved */
-  0,                                        /*     Reserved */
-  SVC_Handler,                              /*     SVC Handler */
-  DebugMon_Handler,                         /*     Debug Monitor Handler */
-  0,                                        /*     Reserved */
-  PendSV_Handler,                           /*     PendSV Handler */
-  SysTick_Handler,                          /*     SysTick Handler */
-  /* Unused */
-};

arm-software/embedded/llvmlibc-support/crt0/CMakeLists.txt
@@ -6,7 +6,7 @@
 #
 
 add_library(crt0 STATIC
-  crt0.c
+  bootcode.cpp
 )
 
 target_include_directories(crt0 PRIVATE
@@ -17,7 +17,7 @@ target_include_directories(crt0 PRIVATE
 # This is the exact same implementation, kept for backwards compatibility with
 # picolibc in testing.
 add_library(crt0-semihost STATIC
-  crt0.c
+  bootcode.cpp
 )
 
 target_include_directories(crt0-semihost PRIVATE

arm-software/embedded/llvmlibc-support/crt0/crt0.c
@@ -1,37 +1,0 @@
-//
-// Copyright (c) 2022-2025, Arm Limited and affiliates.
-//
-// Part of the Arm Toolchain project, under the Apache License v2.0 with LLVM Exceptions.
-// See https://llvm.org/LICENSE.txt for license information.
-// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
-//
-
-#include <stddef.h>
-#include <stdint.h>
-#include <stdlib.h>
-#include <string.h>
-
-#include "platform.h"
-
-extern int main(int argc, char **argv);
-
-extern void _platform_init();
-
-extern char __data_source[];
-extern char __data_start[];
-extern char __data_size[];
-extern char __bss_start[];
-extern char __bss_size[];
-
-void c_startup(void) {
-  memcpy(__data_start, __data_source, (size_t)__data_size);
-  memset(__bss_start, 0, (size_t)__bss_size);
-  _platform_init();
-  _Exit(main(0, NULL));
-}
-
-extern long __stack[];
-__attribute__((naked)) void _start(void) {
-  __asm__("mov sp, %0" : : "r"(__stack));
-  __asm__("b c_startup");
-}

commit cf8abb9f67b5b46547c6bb4a22e9587f5f007302

[ATfE] Fix bootcode not working for ARMv4T (#397)
To fix some of the nightly failing build errors (introduced by the patch
https://github.com/arm/arm-toolchain/pull/394) which was caused by the
ARMv4T variant, stub functions need to be introduced temporarily.

Previously, only ARM architectures 'A', 'R' and 'M' were considered,
therefore the `#if ... #endif` clause would miss any other variants.
When tests are able to run, we can fill in the stub functions to fix any
future failed tests, but up until the tests, this acts to suppress the
build errors.

arm-software/embedded/llvmlibc-support/crt0/bootcode.cpp
@@ -7,6 +7,8 @@
 //
 
 #include <stdint.h>
+#include <stdlib.h> // for exit()
+#include <string.h> // for memcpy(), memset()
 
 #if __ARM_ARCH_PROFILE == 'A' || __ARM_ARCH_PROFILE == 'R'
 #include "exceptions_a.h"
@@ -18,6 +20,24 @@
 #include "memory_m.h"
 #include "misc_m.h"
 #include "system_registers_m.h"
+#else
+// ARMv4T
+// TODO: fill in stub functions once we can start testing LLVM-libc
+namespace bootcode {
+namespace exceptions {
+void setup() noexcept {}
+} // namespace exceptions
+
+namespace memory {
+void enable_cache() noexcept {}
+void setup() noexcept {}
+} // namespace memory
+
+namespace misc {
+void setup() noexcept {}
+} // namespace misc
+
+} // namespace bootcode
 #endif
 
 using namespace bootcode;
@@ -35,6 +55,7 @@ extern char __data_start[];
 extern char __data_size[];
 extern char __bss_start[];
 extern char __bss_size[];
+extern char __stack __attribute__((weak));
 
 #ifdef __ARM_FEATURE_PAUTH
 // Disable pointer authentication, as it isn't enabled until misc::setup meaning